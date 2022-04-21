d8ad5426cf8d4f97f91ba54912c41f215e52c21a
Luis Miguel Rodríguez <101124184+luismiguelrodriguez3@users.noreply.github.com>
2022-04-20 19:58:55 -0500

    campos nuevos: Apellido,Cargo y Clave, ademas intente hacer un inicio de sesio, pero no al momento de la consulta hace la conexion, pero sale error, y como lo queria hacer sin ayuda, no pregunté jeje

---
+++ sistema/src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID;
+    //Nuevos campos: apellido, clave y cargo
+    String nombre,apellido, correo, clave, cargo;
+    
+//Set y Get de los nuevos campos
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
